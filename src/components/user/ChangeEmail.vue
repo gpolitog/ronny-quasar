cad95d143d8b0ac3ed5a8b87e1c4a067fb44605f
Jens Willemsens <jens@jensw.be>
2017-11-28 07:48:37 +0100

    Change email / password basic GUI and vilidation

--- src/components/user/ChangeEmail.vue
+++ src/components/user/ChangeEmail.vue
@@ -1,14 +1,50 @@
 <template>
   <!-- if you want automatic padding use "layout-padding" class -->
   <div class="layout-padding">
-    <h1>Change email</h1>
+    <h1>Mailadres aanpassen</h1>
+    <q-field label="Nieuw mailadres" :label-width="3">
+      <q-input name="email" v-model="email" />
+    </q-field>
+    <q-field label="Herhaal mailadres" :label-width="3">
+      <q-input name="repeat_email" v-model="repeat" />
+    </q-field>
+    <q-btn color="green" :disabled="!emailCheck">Opslaan</q-btn>
+    <q-btn @click="cancelChange">Annuleren</q-btn>
   </div>
 </template>
 
 <script>
+import {
+  QField,
+  QInput,
+  QBtn
+} from 'quasar'
+import isEmail from 'isemail'
+
 export default {
   data () {
-    return {}
+    return {
+      email: '',
+      repeat: ''
+    }
+  },
+
+  computed: {
+    emailCheck () {
+      return isEmail.validate(this.email) && this.email === this.repeat
+    }
+  },
+
+  methods: {
+    cancelChange () {
+      this.$router.replace('/')
+    }
+  },
+
+  components: {
+    QField,
+    QInput,
+    QBtn
   }
 }
 </script>
--- src/components/user/ChangePassword.vue
+++ src/components/user/ChangePassword.vue
@@ -1,14 +1,53 @@
 <template>
   <!-- if you want automatic padding use "layout-padding" class -->
   <div class="layout-padding">
-    <h1>Change password</h1>
+    <h1>Wachtwoord aanpassen</h1>
+    <q-field label="Huidige wachtwoord" :label-width="3">
+      <q-input name="password" v-model="oldPass" type="password" />
+    </q-field>
+    <q-field label="Nieuw wachtwoord" :label-width="3">
+      <q-input name="new_password" v-model="newPass" type="password" />
+    </q-field>
+    <q-field label="Herhaal wachtwoord" :label-width="3">
+      <q-input name="repeat_password" v-model="repeatPass" type="password" />
+    </q-field>
+    <q-btn color="green" :disabled="!passwordCheck">Opslaan</q-btn>
+    <q-btn @click="cancelChange">Annuleren</q-btn>
   </div>
 </template>
 
 <script>
+import {
+  QField,
+  QInput,
+  QBtn
+} from 'quasar'
+
 export default {
   data () {
-    return {}
+    return {
+      oldPass: '',
+      newPass: '',
+      repeatPass: ''
+    }
+  },
+
+  computed: {
+    passwordCheck () {
+      return this.oldPass.length && this.newPass.length && this.repeatPass.length && this.newPass === this.repeatPass
+    }
+  },
+
+  methods: {
+    cancelChange () {
+      this.$router.replace('/')
+    }
+  },
+
+  components: {
+    QField,
+    QInput,
+    QBtn
   }
 }
 </script>
